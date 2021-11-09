e8844ece2258006d424711e8c9eb60f4380caab7
IdrKim <rkdlq9584@gmail.com>
2021-11-09 18:39:04 +0900

    Bot Login

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const { Client, Intents } = require('discord.js');
+const config = require("../config.json");
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(config.BOT_TOKEN);
